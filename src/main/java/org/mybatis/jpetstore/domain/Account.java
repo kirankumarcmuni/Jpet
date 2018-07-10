e4ab5a436dd94efcd72627f18a4c1c47a4381e8a
cmunikirankumar <31480799+kirankumarcmuni@users.noreply.github.com>
2018-07-10 22:15:54 +0530

    Update Account.java
    fix typo

--- src/main/java/org/mybatis/jpetstore/domain/Account.java
+++ src/main/java/org/mybatis/jpetstore/domain/Account.java
@@ -1,4 +1,4 @@
-n/**
+/**
  * Copyright 2010-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
